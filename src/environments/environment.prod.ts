62fa2cbc6402fdd108f6b6a6d206f8f511a924a2
khalil <khalillakhdharatc@gmail.com>
2022-09-02 10:13:17 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  defaultauth: 'firebase',
+  firebaseConfig: {
+    apiKey: "AIzaSyCaWXeLf1dYtJJ8JYdkYJMRbmTGl0ahKTE",
+    authDomain: "mycommerce-28cab.firebaseapp.com",
+    databaseURL: "https://mycommerce-28cab-default-rtdb.firebaseio.com",
+    projectId: "mycommerce-28cab",
+    storageBucket: "mycommerce-28cab.appspot.com",
+    messagingSenderId: "553477085294",
+    appId: "1:553477085294:web:5d97f0da4e87bf1457ae7f",
+    measurementId: "G-TF554DXLXG"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  defaultauth: 'firebase',
+  firebaseConfig: {
+    apiKey: "AIzaSyCaWXeLf1dYtJJ8JYdkYJMRbmTGl0ahKTE",
+    authDomain: "mycommerce-28cab.firebaseapp.com",
+    databaseURL: "https://mycommerce-28cab-default-rtdb.firebaseio.com",
+    projectId: "mycommerce-28cab",
+    storageBucket: "mycommerce-28cab.appspot.com",
+    messagingSenderId: "553477085294",
+    appId: "1:553477085294:web:5d97f0da4e87bf1457ae7f",
+    measurementId: "G-TF554DXLXG"
+  }
+};
+
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
